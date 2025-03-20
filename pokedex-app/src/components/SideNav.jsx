1691d76a0ecc56d5c51d2afc5129c1aa069aff0d
Cristofor <cristofor.doarme@gmail.com>
2025-03-20 14:16:56 +0100

    added detail View and images

--- pokedex-app/src/components/SideNav.jsx
+++ pokedex-app/src/components/SideNav.jsx
@@ -1,19 +1,18 @@
 import {first151Pokemon, getFullPokedexNumber} from '../utils/index.js'
 
-export function SideNav(){
+export default function SideNav(){
   return  (
     <nav>
       
       <div className={'header'}>
         <h1 className={'text-gradient'}>Pokedex</h1>
       </div>
-      
+
       <input />
       {
         first151Pokemon.map((pokemon, index) => {
           return (
-            
-            <button className={'nav-card'}>
+            <button key={index} className={'nav-card'}>
               <p>{getFullPokedexNumber(index)}</p>
               <p>{pokemon}</p>
             </button>
